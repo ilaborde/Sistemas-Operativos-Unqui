8673a14099a438fcb42bedf698891aa81e40e8f5
Ignacio <laborde.ignacio@gmail.com>
2015-11-28 14:03:57 -0300

    updated programs instructions

--- Code/_init_.py
+++ Code/_init_.py
@@ -3,12 +3,10 @@ from Code.factories.kernelFactory import *
 
 def console():
     x = ""
-    kernel.load("program0")
-    kernel.load("program1")
     while True:
         print("Enter program...")
         x = input("program: ")
-
+        kernel.load(x)
 
 if __name__ == '__main__':
     kernel = kernelFactory().selectConfiguration(1)
