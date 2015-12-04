68c9dd209e325080a5c609dd25faa0baefdd5d27
oscar <oscar_master93@hotmail.com>
2015-12-04 01:50:14 -0300

    Finish Commit

--- Code/_init_.py
+++ Code/_init_.py
@@ -11,27 +11,7 @@ if __name__ == '__main__':
     kernel = kernelFactory().selectConfiguration(1)
     console = Console()
     # mock shell
+    # Names of the programs loaded: program0, program1 or program2, to run a program enter : exec programExample
     console.start(kernel)
     console.run()
 
-    # messages = 'This is Console1', 'This is Console2'
-    #
-    # if platform.system() == "Windows":
-    #       new_window_command = "cmd.exe /c start".split()
-    # else:  #XXX this can be made more portable
-    #       new_window_command = "x-terminal-emulator -e".split()
-    #
-    #     # open new consoles, display messages
-    # echo = [sys.executable, "-c",
-    #             "from Code.factories.kernelFactory import * ;"
-    #             "from Code.Console import Console  ;"
-    #             "kernel = kernelFactory().selectConfiguration(1) ;"
-    #             "console = Console() ;"
-    #             "console.start(kernel) ;"
-    #             "console.run() ;"]
-    #
-    # processes = [Popen(new_window_command + echo + [msg]) for msg in messages]
-    #
-    # # wait for the windows to be closed
-    # #for proc in processes:
-    # processes[0].wait()
